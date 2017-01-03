e2384bd9670389fe6410201dabced8653a22e6b7
Dan Buzaid <dan@mytennislessons.com>
2017-01-03 16:02:28 -0700

    mobile image and GA script added

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -5,18 +5,41 @@
     .container
       .row
         .columns.six.text-right
-          img(src="../assets/dhs-mascot.png")
+          img.hidden-xs(src="../assets/dhs-mascot.png")
+          .text-center.visible-xs
+            img(src="../assets/dhs-hat.png")
+
         .columns.six
           h3 The Details
           ul
-            li Portugese Club
+            li
+              | Portugese Club&nbsp;
+              small
+                a(href="https://goo.gl/maps/vShdyXC5KEp" target="_blank") (view map)
             li Saturday, June 17 2017
             li 7:00pm to Midnight
-            li Ticket Price: $45
             li Open Bar & Buffet Style
+            li.helped Early Ticket Price: $40
+            li.helper $50 after March 31st
+            li.center
+              a(href="https://www.eventbrite.com/e/danbury-high-school-2007-class-reunion-tickets-30853663137?ref=elink", class="button button-primary")
+                | Purchase Tickets
 </template>
 
 <style lang="scss" scoped>
+  li.helped {
+    margin-bottom: 5px;
+  }
+  li.helper {
+    list-style: none;
+    text-align: center;
+    font-size: 13px;
+    font-weight: bold;
+  }
+  li.center {
+    list-style: none;
+    text-align: center;
+  }
   h1 {
     font-size:9vw;
     text-align: center;
@@ -30,9 +53,17 @@
   }
   h3 {
     font-weight: 800;
-    margin-top:120px;
+    margin-top:30px;
     font-size: 3rem;
     color: #0D47A1;
+  }
+  .button-primary {
+    background: #F5701C;
+    border-color: #F5701C;
+    &:hover {
+      background: darken(#F5701C, 10%);
+      border-color: #F5701C;
+    }
   }
   ul {
     width: 320px;
@@ -50,6 +81,9 @@
   img {
     padding-right: 20px;
     width: 200px;
+  }
+  .visible-xs {
+    display: none;
   }
   @media screen and (max-width: 560px) {
     h1 {
@@ -76,8 +110,11 @@
         font-size:1rem;
       }
     }
-    img {
+    .hidden-xs {
       display: none;
+    }
+    .visible-xs {
+      display: block;
     }
   }
 
